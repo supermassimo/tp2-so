4b793673864a00b5c138334d837d9a5455742dba
supermassimo <massimocantugarcia@gmail.com>
2021-06-21 14:32:03 -0300

    Update quadratic.asm

--- Userland/UserModule/standard_library/quadratic.asm
+++ Userland/UserModule/standard_library/quadratic.asm
@@ -9,11 +9,11 @@ section .text
 ;root2=(-b-d) /(2.0*a);
 
 ;MOVQ [to here], [from here] move float
-;DIVQ [div this], [by this] divide float
+;DIVSS [div this], [by this] divide float
 ;MULQ [mult this], [by this] multiply float
 ;SQRTSS [destination], [sqrt of this] square root float
-;ADDQ [this], [plus this] add floats
-;SUBQ [this], [minus this] substract floats
+;ADDSS [this], [plus this] add floats
+;SUBSS [this], [minus this] substract floats
 ;CVTSI2SS [float reg], [int reg] moves a value from an integer register to a float one
 ;XORPS [float reg 1], [float reg 2] XOR instruction for float registers
 
@@ -44,7 +44,7 @@ getQuadratic:
 
     ;[a][b][c][b*b][a*c*4][a*2][2][]
 
-    subq xmm3, xmm4    ;substract (b*b) minus (4*a*c) (stored in xmm3)
+    subss xmm3, xmm4    ;substract (b*b) minus (4*a*c) (stored in xmm3)
 
     ;[a][b][c][(b*b)-(a*c*4)][a*c*4][a*2][2][]
 
@@ -60,18 +60,18 @@ getQuadratic:
     ;[a][b][c][(b*b)-(a*c*4)][a*c*4][a*2][d][]
 
     xorps xmm3, xmm3    ;set xmm3 to 0
-    subq xmm3, xmm1    ;set xmm3 to -b
+    subss xmm3, xmm1    ;set xmm3 to -b
     movq xmm4, xmm3    ;set xmm4 to -b
 
     ;[a][b][c][-b][-b][a*2][d][]
 
-    addq xmm3, xmm4    ;add sqrt((b*b)-(a*c*4)) to -b
-    subq xmm4, xmm4    ;substract sqrt((b*b)-(a*c*4)) from -b
+    addss xmm3, xmm4    ;add sqrt((b*b)-(a*c*4)) to -b
+    subss xmm4, xmm4    ;substract sqrt((b*b)-(a*c*4)) from -b
 
     ;[a][b][c][-b+d][-b-d][a*2][d][]
 
-    divq xmm3, xmm5    ;divide (-b+d) by (a*2)
-    divq xmm4, xmm5    ;divide (-b-d) by (a*2)
+    divss xmm3, xmm5    ;divide (-b+d) by (a*2)
+    divss xmm4, xmm5    ;divide (-b-d) by (a*2)
 
     ;[a][b][c][root 1][root 2][a*2][d][]
 
@@ -91,11 +91,11 @@ singleRoot:
     ;[a][b][c][(b*b)-(a*c*4)][a*c*4][a*2][][]
 
     xorps xmm3, xmm3    ;set xmm3 to zero
-    subq xmm3, xmm1    ;set xmm3 to -b
+    subss xmm3, xmm1    ;set xmm3 to -b
 
     ;[a][b][c][-b][a*c*4][a*2][][]
 
-    divq xmm3, xmm5    ;divide (-b) by (a*2)
+    divss xmm3, xmm5    ;divide (-b) by (a*2)
 
     ;[a][b][c][root][a*c*4][a*2][][]
 
@@ -104,3 +104,11 @@ singleRoot:
     mov rax, 1          ;set rax to 1, we found 1 root
 
     jmp end
+
+section .data
+  minusFour dw 4
+
+section .bss
+a: resq 1
+  b: resq 1
+  c: resq 1
